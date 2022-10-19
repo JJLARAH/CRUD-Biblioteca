ea7081f04fa265c9e424c8666e82913683158669
Johany Lara <joseph.larahi@gmail.com>
2022-10-19 16:53:11 -0500

    Modificación del modelo Book, se agregarón las relaciones con las tablas Author y Genre

--- models/Book.php
+++ models/Book.php
@@ -12,9 +12,12 @@ use Yii;
  * @property int $pagecount
  * @property int $id_author
  * @property int $id_genre
- * @property string $cover
+ * @property string|null $cover
+ *
+ * @property BookAuthor $author
+ * @property Borrows[] $borrows
+ * @property BookGenre $genre
  */
-
 class Book extends \yii\db\ActiveRecord
 {
     public $image;
@@ -33,12 +36,14 @@ class Book extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['title','pagecount','id_author','id_genre'], 'required'],
+            [['title', 'pagecount', 'id_author', 'id_genre'], 'required'],
+            [['pagecount', 'id_author', 'id_genre'], 'integer'],
             [['title'], 'string', 'max' => 255],
             [['pagecount'], 'number', 'max' => 9999],
-            [['id_author'], 'number', 'max' => 11],
-            [['id_genre'], 'number', 'max' => 11],
+            [['id_author'], 'exist', 'skipOnError' => true, 'targetClass' => BookAuthor::class, 'targetAttribute' => ['id_author' => 'id_author']],
+            [['id_genre'], 'exist', 'skipOnError' => true, 'targetClass' => BookGenre::class, 'targetAttribute' => ['id_genre' => 'id_genre']],
             [['image'], 'file', 'extensions' => 'jpg,jpeg,png,webp']
+
         ];
     }
 
@@ -55,5 +60,35 @@ class Book extends \yii\db\ActiveRecord
             'id_genre' => 'Genre',
             'image' => 'Cover',
         ];
+    }
+
+    /**
+     * Gets query for [[Author]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAuthor()
+    {
+        return $this->hasOne(BookAuthor::class, ['id_author' => 'id_author']);
+    }
+
+    /**
+     * Gets query for [[Borrows]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBorrows()
+    {
+        return $this->hasMany(Borrows::class, ['id_book' => 'id_book']);
+    }
+
+    /**
+     * Gets query for [[Genre]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getGenre()
+    {
+        return $this->hasOne(BookGenre::class, ['id_genre' => 'id_genre']);
     }
 }
