6d9a5ab4a11477b936dc18d5ec237c4b859047cb
Johany Lara <joseph.larahi@gmail.com>
2022-10-20 15:43:51 -0500

    Creación del módelo BorrowSearch

--- models/BorrowSearch.php
+++ models/BorrowSearch.php
@@ -88,4 +88,59 @@ class BorrowSearch extends Borrow
 
         return $dataProvider;
     }
+
+     /**
+     * Creates data provider instance with search query applied for a user
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function searchHistory($params)
+    {
+        $query = Borrow::find()->where(['users.id_user' => 3]);
+
+        $query->joinWith(['book', 'user']);
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+            'pagination' => ['pageSize' => 5]
+        ]);
+
+        $dataProvider->sort->attributes['book'] = [
+            'asc' => ['books.title' => SORT_ASC],
+            'desc' => ['books.title' => SORT_DESC],
+        ];
+
+        $dataProvider->sort->attributes['user'] = [
+            'asc' => ['users.name' => SORT_ASC],
+            'desc' => ['users.name' => SORT_DESC],
+        ];
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id_borrow' => $this->id_borrow,
+            'id_user' => $this->id_user,
+            'id_book' => $this->id_book,
+            'date_borrow' => $this->date_borrow,
+            'date_return' => $this->date_return,
+        ]);
+
+        $query->andFilterWhere(['like', 'books.title', $this->book])
+            ->andFilterWhere(['like', 'users.name', $this->user])
+            ->andFilterWhere(['like', 'date_borrow', $this->date_borrow])
+            ->andFilterWhere(['like', 'date_return', $this->date_return]);
+
+        return $dataProvider;
+    }
 }
