c6abe55faa4d69aac20bf0c68096c7d2ae25f131
Johany Lara <joseph.larahi@gmail.com>
2022-10-21 16:41:05 -0500

    Agrege botón crear autor

--- views/book/index.php
+++ views/book/index.php
@@ -19,6 +19,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <p>
         <?= Html::a('Create Book', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Create Author', ['author/create'], ['class' => 'btn btn-primary']) ?>
     </p>
 
     <?php // echo $this->render('_search', ['model' => $searchModel]); 
