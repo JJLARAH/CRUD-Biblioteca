c5fa09dc416f33dd7e492a51487bffe25a214f5d
Johany Lara <joseph.larahi@gmail.com>
2022-10-21 00:12:57 -0500

    Agrege label para dropDown de Author y Genre

--- views/book/_form.php
+++ views/book/_form.php
@@ -4,7 +4,7 @@ use app\models\Book;
 use app\models\BookAuthor;
 use app\models\BookGenre;
 use yii\helpers\Html;
-use yii\widgets\ActiveForm; 
+use yii\widgets\ActiveForm;
 use \yii\helpers\ArrayHelper;
 
 /** @var yii\web\View $this */
@@ -21,21 +21,31 @@ use \yii\helpers\ArrayHelper;
 
         <?= $form->field($model, 'pagecount')->textInput(['type' => 'number', 'maxlength' => true, 'placeholder' => 'Total number of pages', 'required' => '']) ?>
 
-        <?php $var = ArrayHelper::map(BookAuthor::find()->all(), 'id_author', 'name'); ?>
-        <?= $form->field($model, 'id_author')->dropDownList($var, ['id_author' => 'Select Author']); ?>
+        <?php $var = ArrayHelper::map(BookAuthor::find()
+            ->all(), 'id_author', 'surname','name');
+        ?>
+        <?= $form->field($model, 'author')
+            ->dropDownList($var, ['id_author' => 'Select Author']);
+        ?>
 
-        <?php $var = ArrayHelper::map(BookGenre::find()->all(), 'id_genre', 'genre'); ?>
-        <?= $form->field($model, 'id_genre')->dropDownList($var, ['id_genre' => 'Select Genre']); ?>
-        
-        <div class="col-sm-1">
-            <?= Html::img($model->cover, ['width' => '240px']); ?>
+        <?php $var = ArrayHelper::map(BookGenre::find()
+            ->all(), 'id_genre', 'genre');
+        ?>
+        <?= $form->field($model, 'genre')
+            ->dropDownList($var, ['id_genre' => 'Select Genre'])
+        ?>
+
+        <div class="col-sm-auto">
+            <?= Html::img($model->cover, ['width' => '200px']); ?>
         </div>
-
-        <?= $form->field($model, 'image')->fileInput() ?>
+        <div class="col-sm-auto">
+            <?= $form->field($model, 'image')->fileInput() ?>
+        </div>
 
         <div class="form-group">
             <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
         </div>
+        
 
     </div>
     <?php ActiveForm::end(); ?>
