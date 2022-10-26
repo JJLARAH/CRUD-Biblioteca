b4defef4fdb9f5d31c2efe4062d53f296c01ac2f
Johany Lara <joseph.larahi@gmail.com>
2022-10-25 19:01:47 -0500

    Cambie tamaño de string para campo location

--- migrations/m221025_231411_create_users_table.php
+++ migrations/m221025_231411_create_users_table.php
@@ -18,7 +18,7 @@ class m221025_231411_create_users_table extends Migration
             'surname' => $this->string(45)->notNull(),
             'gender' => $this->string(20),
             'phonenum' => $this->string(20)->notNull(),
-            'location' => $this->string(20)->notNull(),
+            'location' => $this->string(255)->notNull(),
             'photo' => $this->string(2500)
         ]);
     }
